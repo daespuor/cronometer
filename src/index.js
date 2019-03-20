525dc18f89b489758a1105bfb9cfc70d78486549
Daniel Esteban Puerta <daespuor@hotmail.com>
2019-03-19 21:48:08 -0500

    Class component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Cronometer from './cronometer/cronometer.hooks';
+import Cronometer from './cronometer/cronometer.component';
 
 ReactDOM.render(<Cronometer />, document.getElementById('root'));
